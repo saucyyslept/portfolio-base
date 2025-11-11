5867a0fda5500a63cde4628ecd3ac12b53760253
saucyyslept <batmanarriveshome@gmail.com>
2025-11-11 15:19:11 +0530

    Add initial Python script with greeting function

---
+++ main.py
@@ -0,0 +1,9 @@
+# My first GitHub project
+# Author: Atharva
+
+def greet():
+    print("Hello, GitHub! 👋")
+    print("This is my first commit and project.")
+
+if __name__ == "__main__":
+    greet()
